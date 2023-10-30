274f1706cd81cebfb362de4f129cb73c1069a24b
Rick Arcanjo <ricardo.cassettari07@gmail.com>
2023-10-29 23:15:38 -0300

    exercicio realizado

---
+++ script.js
@@ -0,0 +1,5 @@
+const nome = prompt('Qual é o seu nome ? ');
+const sobrenome = prompt('Qual é o seu sobrenome ? ');
+
+alert(`Seu nome completo é: ${nome} ${sobrenome}`);
+alert(`Seu nome de catálogo é: ${sobrenome.toUpperCase()}, ${nome}`);
